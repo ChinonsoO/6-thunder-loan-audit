2cc3e7ab575cd37bb63ae4b2846b3d78bea23960
ChinonsO <Chinonso.oragwam@ucalgary.ca>
2025-02-18 07:27:21 -0700

    initial intro

--- src/interfaces/IPoolFactory.sol
+++ src/interfaces/IPoolFactory.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.20;
 
+
 interface IPoolFactory {
+    // q - Why are we using Tswap here?
     function getPool(address tokenAddress) external view returns (address);
 }
